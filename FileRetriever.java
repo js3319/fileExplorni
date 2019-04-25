1b8ba0f1d1d1d10a8e6907d4a862be942996bdb5
js3319 <49924544+js3319@users.noreply.github.com>
2019-04-25 14:46:27 -0700

    now moves files successfully

--- FileRetriever.java
+++ FileRetriever.java
@@ -1,9 +1,67 @@
 import java.io.File;
+import java.io.IOException;
 public class FileRetriever {
+	//selects files in preparation for modifiers 
+	File fileName;
+	
+	//File fileLoc = new File(fileName);
 	public static final String directory = "C:\\Users";
-	String fileName = "";
 	
+	public FileRetriever(String name) throws IOException {
+		
+		createFolder(name);
+		System.out.println(fileName.getParent()+"/"+getFileExtension(name));
 	
+		moveFile(name, (fileName.getParent()+"/"+getFileExtension(name)));
+	}
 	
-	File fileLoc = new File(fileName);
+	public String getFileExtension(String name) {
+		String extName = null;
+		int extLoc;
+		for(int x=0;x<name.length()-1;x++) {
+			if(name.substring(x,x+1).equals(".")) {
+				extName=name.substring(x+1);
+			
+			}
+		}
+		return extName;
+	}
+	
+	public void createFolder(String name) {
+		fileName=new File(name);	
+		//fileName is the original input
+		String parent = fileName.getParent()+"/";
+		//gets parent directory
+		String folderName = parent + getFileExtension(name);
+		//generates foldername based on parent directory and file extension name
+		System.out.println (folderName);
+		new File(folderName).mkdirs();
+		System.out.println(getFileExtension(name));
+	}
+	
+	public void moveFile(String original, String newLoc) {
+		File select= new File(original);
+		//System.out.println((newLoc+"/"+original));
+		
+	
+		select.renameTo(new File(newLoc+actualFileName(original)));
+		
+	}
+	public String actualFileName(String sample) {
+		int count = 2;
+		for(int x=sample.length();x>0;x--) {
+			if (sample.substring(x-1,x).equals("/")){
+				count--;
+				if(count ==0) {
+					return sample.substring(x);
+				}
+			
+			}
+		}
+		return null;
+	}
+
+	
+		
+	
 }
--- Runner.java
+++ Runner.java
@@ -2,10 +2,17 @@ import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.awt.Desktop;
 import java.awt.EventQueue;
 import java.io.File;
+import java.io.IOException;
 public class Runner {
 String input;
+String change;
+Desktop desktop;
+File file;
 public Runner() {
 	EventQueue.invokeLater(new Runnable() {
 		@Override
@@ -19,14 +26,66 @@ public Runner() {
 	});
 } 
 	public static void main(String[]args) {
-		Runner run = new Runner();
+		Runner run = new Runner();			
+		
 	}
 
 	
 	public void getUserInput() {
-		String ni = JOptionPane.showInputDialog("Enter ni");	
+		//mac os is forward slash /users/js/xxxxxxxx
+		//ex. "/Users/js/Downloads/IMG_1388.jpg/"
+		//perhaps put hints to help user guide, or adapt gui to have user select directory path
+		input = JOptionPane.showInputDialog("Enter Directory");	
+		
+		desktop = Desktop.getDesktop();
+		if(Desktop.isDesktopSupported()) {
+		//URI is essentially url, this segment opens 
+//		URI uri;
+//		try {
+//			uri = new URI("https://www.reddit.com/");
+//			desktop.browse(uri);
+//		} catch (URISyntaxException | IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+		File file = new File (input);
+		try {
+			desktop.open(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(input);
+	//	System.out.println(desktop);
+		if(file.isDirectory()) {
+			System.out.println("is directory");
+		}
+		}
+		else{
+			System.out.println("desktop not supported!");
+		}
+		
+	//	change = JOptionPane.showInputDialog("Enter Desired Change");	
 	}
 	public void start() {
+		//put methods here
 		getUserInput();
+		//checkInput();
+		try {
+			FileRetriever finder= new FileRetriever(input);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	public void checkInput() {
+		//	if(input)
+		
+		}
+	
+	
 }
+
+	
+
